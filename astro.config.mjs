d70c83c8673012ab55d6d592fbd9a4cf8173c343
DynoW <104089032+DynoW@users.noreply.github.com>
2023-09-09 11:32:21 +0300

    modified:   astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,8 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://cuza.pages.dev',
+    pages: {
+        'page.astro': 'page.html'
+    },
     integrations: [sitemap()],
 });
