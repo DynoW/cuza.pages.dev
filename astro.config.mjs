52303c1ecf112ed49e74e67150e107fc9b53c4a0
DynoW <104089032+DynoW@users.noreply.github.com>
2023-09-09 22:59:34 +0300

    modified:   .vscode/extensions.json 	modified:   astro.config.mjs 	modified:   package-lock.json 	modified:   package.json 	modified:   public/assets/css/styles.min.css 	modified:   src/components/Footer.astro 	modified:   src/layouts/Layout.astro 	modified:   src/pages/index.astro 	new file:   tailwind.config.cjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://cuza.pages.dev',
-    trailingSlash: 'never',
-    build: {
-        format: 'file'
-    },
-    integrations: [sitemap()],
+  site: 'https://cuza.pages.dev',
+  trailingSlash: 'never',
+  build: {
+    format: 'file'
+  },
+  integrations: [sitemap(), tailwind()]
 });
