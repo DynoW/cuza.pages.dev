14d2ef0356fcc2a38e42dd30aebde5270fefd64a
DynoW <104089032+DynoW@users.noreply.github.com>
2023-09-15 18:31:14 +0300

    modified:   astro.config.mjs     modified:   package-lock.json 	modified:   package.json 	modified:   src/layouts/Layout.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,14 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import prefetch from "@astrojs/prefetch";
+import compress from "astro-compress";
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://cuza.pages.dev',
   trailingSlash: 'never',
   build: {
     format: 'file'
   },
-  integrations: [sitemap(), tailwind(), react(), prefetch()]
+  integrations: [sitemap(), tailwind(), react(), prefetch(), compress()]
 });
