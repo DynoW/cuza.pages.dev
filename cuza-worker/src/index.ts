b8e98fd524d6067f889b5f3593809df4d78959cf
DynoW <104089032+DynoW@users.noreply.github.com>
2024-12-29 11:09:59 +0200

    modified:   cuza-worker/src/index.ts

--- cuza-worker/src/index.ts
+++ cuza-worker/src/index.ts
@@ -1,7 +1,7 @@
 import { Octokit } from "@octokit/rest";
 
 const corsHeaders = {
-    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Origin': 'https://cuza.pages.dev',
     'Access-Control-Allow-Methods': 'POST, OPTIONS',
     'Access-Control-Allow-Headers': 'Content-Type, Authorization'
 };
