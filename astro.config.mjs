59015ce712a1c49d957029c41350e3d250664039
DynoW <104089032+DynoW@users.noreply.github.com>
2025-08-15 10:59:18 +0300

    Test auto fetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import sitemap from '@astrojs/sitemap';
 import react from "@astrojs/react";
 import compress from "astro-compress";
 
+const COUNTDOWN_DATE = new Date('2024-12-31T23:59:59').getTime();
+
 export default defineConfig({
   site: 'https://cuza.pages.dev',
   trailingSlash: 'never',
@@ -16,6 +18,9 @@ export default defineConfig({
     assets: 'file'
   },
   vite: {
+    define: {
+      'import.meta.env.COUNTDOWN_DATE': JSON.stringify(COUNTDOWN_DATE)
+    },
     build: {
       rollupOptions: {
         output: {
