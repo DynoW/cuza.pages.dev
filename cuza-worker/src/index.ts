fb4277eb53d287c2c0876c5a5873e90182fc07d9
DynoW <104089032+DynoW@users.noreply.github.com>
2025-12-19 16:57:04 +0200

    Update dependencies and add comments for dark reader

--- cuza-worker/src/index.ts
+++ cuza-worker/src/index.ts
@@ -2,8 +2,8 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.get('/ping', (c) => {
+  return c.text('Pong!')
 })
 
 export default app
