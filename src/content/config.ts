e15e7c93b3799f32b398914ad738e0b951245ff7
DynoW <104089032+DynoW@users.noreply.github.com>
2025-12-21 09:01:22 +0200

    Improve infoCollection

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,9 @@
-import { z, defineCollection } from 'astro:content';
+import { defineCollection } from 'astro:content';
+import { glob, file } from 'astro/loaders'; // Not available with legacy API
+import { z } from 'astro/zod';
 
 const infoCollection = defineCollection({
-    type: 'content',
+    loader: glob({ pattern: "**/*.md", base: "./src/content/info" }),
     schema: z.object({
         title: z.string(),
         description: z.string()
