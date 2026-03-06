61638b0b10c5a140c0e43e99ba92cc985f88499c
DynoW <104089032+DynoW@users.noreply.github.com>
2026-03-06 20:35:11 +0200

    Deploy funtions

---
+++ functions/api/[[route]].ts
@@ -0,0 +1,9 @@
+import { Hono } from 'hono';
+import { handle } from 'hono/cloudflare-pages';
+import { type Bindings, registerRoutes } from '../../cuza-worker/src/app';
+
+const app = new Hono<{ Bindings: Bindings }>().basePath('/api');
+
+registerRoutes(app as any);
+
+export const onRequest = handle(app);
