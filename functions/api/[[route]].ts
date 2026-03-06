813b09e430474f9c0954c6fb92cba6da00407e0c
DynoW <104089032+DynoW@users.noreply.github.com>
2026-03-06 21:07:07 +0200

    Refactor basePath initialization for Hono app

--- functions/api/[[route]].ts
+++ functions/api/[[route]].ts
@@ -2,7 +2,9 @@ import { Hono } from 'hono';
 import { handle } from 'hono/cloudflare-pages';
 import { type Bindings, registerRoutes } from '../../cuza-worker/src/app';
 
-const app = new Hono<{ Bindings: Bindings }>().basePath('/api');
+const app = new Hono<{ Bindings: Bindings }>();
+
+app.basePath('/api');
 
 registerRoutes(app as any);
 
